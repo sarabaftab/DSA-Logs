fe170e24d527ed1795b6ecd4da39b930d67e543b
Sarab <sarabfsd@gmail.com>
2025-03-17 03:35:50 -0500

    solution added

---
+++ containsDuplicate.py
@@ -0,0 +1,19 @@
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+#Example 1:
+
+#Input: nums = [1,2,3,1]
+
+#Output: true
+
+#Explanation:
+
+#The element 1 occurs at the indices 0 and 3.
+
+def contains_duplicate(nums):
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
